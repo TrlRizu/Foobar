a05e04ebca250e47e932eb93a7131f683e432d44
rizu <trl.rizu2020@gmail.com>
2022-12-02 15:57:15 +0800

    new file:   Fuel_injection.py

---
+++ Fuel_injection.py
@@ -0,0 +1,17 @@
+def solution(n):
+    n = int(n)
+    i = 0
+    while n != 1:
+        if (n % 2 == 0):
+            n /= 2   
+        elif (n % 4 == 1 or n == 3):
+            n -= 1   
+        else: 
+            n += 1     
+        i += 1
+    return i
+
+
+
+
+
